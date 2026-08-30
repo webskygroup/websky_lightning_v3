9da783a8808dce067795da686f9fe8fdb2414c8f
websky group <webskygroup@gmail.com>
2026-08-30 12:39:07 +0330

    Show animated green pre-generation progress

--- websky_lightning/upload/admin/controller/extension/module/websky_lightning.php
+++ websky_lightning/upload/admin/controller/extension/module/websky_lightning.php
@@ -1,7 +1,7 @@
 <?php
 class ControllerExtensionModuleWebskyLightning extends Controller {
     private $error = array();
-    private $version = '1.16.5';
+    private $version = '1.16.6';
     private $version_extension = 'websky_lightning_v3';
     private $download_url = 'https://github.com/webskygroup/websky_lightning_v3/releases/latest/download/websky_lightning.ocmod.zip';
 
